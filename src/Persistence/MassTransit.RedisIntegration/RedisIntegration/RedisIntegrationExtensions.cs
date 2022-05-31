e9352fe1b7d49aca8b521eaab127d80b12e297b7
Plamen Pilev <Plamen.Pilev@geniussports.com>
2022-05-31 17:42:16 +0300

    feat(redis): expose extenison method for event correlation configurator

---
+++ src/Persistence/MassTransit.RedisIntegration/RedisIntegration/RedisIntegrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MassTransit.RedisIntegration
+{
+    using MassTransit.RedisIntegration.Saga;
+    using System;
+
+    public static class RedisIntegrationExtensions
+    {
+        public static IEventCorrelationConfigurator<TSaga, TMessage> CorrelateByRedisKey<TSaga, TMessage>(
+            this IEventCorrelationConfigurator<TSaga, TMessage> configurator,
+            Func<ConsumeContext<TMessage>, string> selector)
+            where TSaga : class, SagaStateMachineInstance, IRedisSagaKey
+            where TMessage : class
+        {
+            return configurator.CorrelateBy(x => "", context => selector(context));
+        }
+    }
+}
